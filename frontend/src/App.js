4f9f992a5bc861d8428412bcb8c834e58d2491d0
Pathu2 <pathubhai@kgpian.iitkgp.ac.in>
2023-10-27 00:41:51 +0530

    Atlas Added and Schedule page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import About from './pages/About';
 import SmTeam from './pages/SmTeam';
 import Gallery from './pages/Gallery';
 import Register from './pages/Register';
+import Schedule from './pages/Schedule';
 
 function App() {
   return (
@@ -16,12 +17,13 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         {EventData.map((event) => (
-          <Route path={event.link} element={<Event event={event} />} />
+          <Route path={`${event.link}`} element={<Event event={event} />} />
         ))}
         <Route path="/about" element={<About />} />
         <Route path='/team' element={<SmTeam />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path='/register' element={<Register />} />
+        <Route path='/schedule' element={<Schedule />}/>
       </Routes>
     </BrowserRouter>
   );
